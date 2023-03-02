0739629bd423cc2838e425f3b231fc8747b20607
ainoal <77739788+ainoal@users.noreply.github.com>
2023-03-02 17:56:17 +0200

    initial commit

---
+++ client.py
@@ -0,0 +1,3 @@
+#https://docs.python.org/3/library/xmlrpc.client.html#module-xmlrpc.client
+
+
---
+++ server.py
@@ -0,0 +1,5 @@
+# https://docs.python.org/3/library/xmlrpc.server.html
+
+import xmlrpc
+
+print("test")

16ce649801028d650711ab9f90df78efaf06950b
ainoal <77739788+ainoal@users.noreply.github.com>
2023-03-03 14:54:21 +0200

    Run server and client with test program

--- client.py
+++ client.py
@@ -1,4 +1,8 @@
 #https://docs.python.org/3/library/xmlrpc.client.html#module-xmlrpc.client
 
-import xmlrpc
+import xmlrpc.client
 
+# Test program to run the client and server
+with xmlrpc.client.ServerProxy("http://localhost:8000/") as proxy:
+    print("3 is even: %s" % str(proxy.is_even(3)))
+    print("100 is even: %s" % str(proxy.is_even(100)))
--- server.py
+++ server.py
@@ -1,5 +1,16 @@
-# https://docs.python.org/3/library/xmlrpc.server.html
+# To execute a client-server program: first run the server application and
+# leave the server running, listening to the right port.
+# After that, you can run client program(s) that make requests to the server.
 
-import xmlrpc
+from xmlrpc.server import SimpleXMLRPCServer
 
-print("test")
+def is_even(n):
+    if (n % 2 == 0):
+        return True
+    else:
+        return False
+
+server = SimpleXMLRPCServer(("localhost", 8000))
+print("Listening on port 8000")
+server.register_function(is_even, "is_even")
+server.serve_forever()

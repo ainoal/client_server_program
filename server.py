cbde6ff208e4364e4d9d3391430fbb47883f09df
ainoal <77739788+ainoal@users.noreply.github.com>
2023-03-09 14:45:13 +0200

    Append note to an existing topic

--- server.py
+++ server.py
@@ -16,32 +16,36 @@ def main():
     print("Listening on port 8000")
     server.register_function(is_even, "is_even")
     server.register_function(new_entry, "new_entry")
-    server.register_function(get_tree, "get_tree")
+    server.register_function(search_topic, "search_topic")
 
     # TODO: try-catch for KeyboardInterrupt
     server.serve_forever()
     return None
 
-def get_tree():
-    tree = ET.parse("db.xml")
-    root = tree.getroot()
-    return root
+def search_topic(root, topic):
+    str_find = ".//topic[@name='" + topic + "']"
+    elem = root.find(str_find)
+    return elem
 
 def new_entry(topic, txt, timestamp):
-    #data = get_tree()
     tree = ET.parse("db.xml")
     data = tree.getroot()
-
     print(data.tag)
 
+    # Search for the topic in the xml file.
+    topic_found = search_topic(data, topic)
+
     # Append the new entry to the XML file.
-    # TODO: make the XML style more beautiful.
-    note = ET.SubElement(data, "topic", name=topic)
+    if topic_found==None:
+        tpc = ET.SubElement(data, "topic", name=topic)
+        note = ET.SubElement(tpc, "note", name="Note 1")
+    else:
+        note = ET.SubElement(topic_found, "note", name="Note x")
     ET.SubElement(note, "text").text = txt
     ET.SubElement(note, "timestamp").text = timestamp
     write_xml(data)
 
-    # TODO: Return information about the XML file.
+    # TODO: Return something that makes sense.
     temp_ret = 3
     return temp_ret
 
@@ -49,7 +53,7 @@ def new_entry(topic, txt, timestamp):
 # https://stackoverflow.com/questions/749796/pretty-printing-xml-in-python/38573964#38573964
 def write_xml(root):
     xml_string = xml.dom.minidom.parseString(ET.tostring(root)).toprettyxml()
-    xml_string = '\n'.join([s for s in xml_string.splitlines() if s.strip()])
+    xml_string = "\n".join([s for s in xml_string.splitlines() if s.strip()])
     f = open("db.xml", "w")
     f.write(xml_string)
     f.close()
